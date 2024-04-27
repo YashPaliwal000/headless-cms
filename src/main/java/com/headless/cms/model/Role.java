290b2706afb1b49063889334551d2ab0530a3eed
Yash Paliwal <yash@holidify.com>
2024-04-27 19:46:22 +0530

    configured proper spring security for session management and authentication Using JWT based authentication

---
+++ src/main/java/com/headless/cms/model/Role.java
@@ -0,0 +1,6 @@
+package com.headless.cms.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/headless/cms/model/User.java
+++ src/main/java/com/headless/cms/model/User.java
@@ -1,24 +1,61 @@
 package com.headless.cms.model;
 
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Document(collection = "users")
 @Data
+@Builder
 @NoArgsConstructor 
 @AllArgsConstructor 
-public class User {
+public class User implements UserDetails {
 
     public static final String SEQUENCE_NAME = "users_sequence";
 
     @Id
     private Long id;
     private String username;
+    private String firstname;
+    private String lastname;
     private String password;
     private String email;
-    private String role;
+    private Role role;
+    private String status = "ACTIVE";
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
